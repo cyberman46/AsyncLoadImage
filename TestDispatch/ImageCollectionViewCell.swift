524de5773a60219c100c397893ca8922cba12b61
cyberman46 <akekapon27@gmail.com>
2017-09-19 09:33:30 +0700

    code async load image

---
+++ TestDispatch/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  TestDispatch
+//
+//  Created by Akekapon Suebprom on 9/19/2560 BE.
+//  Copyright © 2560 kratostracking. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgWallpaper: UIImageView!
+}
--- TestDispatch/ViewController.swift
+++ TestDispatch/ViewController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    
+    var imageLoad : [String] = ["https://newevolutiondesigns.com/images/freebies/space-wallpaper-1.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-2.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-3.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-4.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-5.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-6.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-7.jpg",
+                                    "https://newevolutiondesigns.com/images/freebies/space-wallpaper-8.jpg"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageLoad.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:ImageCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCell
+        //cell.imgWallpaper.image = #imageLiteral(resourceName: "loadingdots")
+        
+        DispatchQueue.global().async {
+            let url = URL(string:self.imageLoad[indexPath.row])
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            DispatchQueue.main.async {
+                cell.imgWallpaper.image = UIImage(data: data!)
+            }
+        }
+        
+        
+        
+        
+        
+        return cell
     }
 
 
